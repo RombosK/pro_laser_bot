c6b0564a481071007f15e468839633bc8811447d
Roman <rombos1978@gmail.com>
2023-10-20 14:33:10 +0300

    ver.2.1

--- config_bd/models/user.py
+++ config_bd/models/user.py
@@ -8,63 +8,66 @@ from sqlalchemy.orm import Mapped, mapped_column, relationship
 from ..models.base import BaseModel, UserStatus
 from ..models.black_list import BlackList
 
+
 class User(BaseModel):
     """
         Модель пользоателя
     """
-    
+
     __tablename__ = 'user'
 
     id = mapped_column(BigInteger, primary_key=True)
 
     # Имя
     first_name: Mapped[str] = mapped_column(
-            String(30),
-            )
-    
+        String(30),
+    )
+
     # Фамилия, может быть NULL
     last_name: Mapped[Optional[str]] = mapped_column(
-            String(50),
-            nullable=True,
-            )
-    
+        String(50),
+        nullable=True,
+    )
+
+    # Phone number
+    number: Mapped[str] = mapped_column(BigInteger, nullable=True)
+
     # Статус выбирается из возможных вариантов модели UserStatus
     status: Mapped[UserStatus] = mapped_column()
 
     # Статус активности клиента, может быть NULL
     is_active: Mapped[bool] = mapped_column(
-            Boolean,
-            default=True,
-            )
+        Boolean,
+        default=True,
+    )
 
     # Поле для дополнительной информации
     info: Mapped[Optional[str]] = mapped_column(
-            Text,
-            nullable=True,
-            )
-    
+        Text,
+        nullable=True,
+    )
+
     # Дата создания, не может быть пустой, по умолчанию текущее время создания
     created_at: Mapped[datetime] = mapped_column(
-            DateTime,
-            nullable=False,
-            default=func.now(),
-            )
+        DateTime,
+        nullable=False,
+        default=func.now(),
+    )
 
     # Дата изменения, может быть пустой
     edited_at: Mapped[Optional[datetime]] = mapped_column(
-            DateTime,
-            nullable=True,
-            server_default=func.current_timestamp(),
-            )
+        DateTime,
+        nullable=True,
+        server_default=func.current_timestamp(),
+    )
 
     # Признак, что запись удалена из системы, по умолчанию False 
     is_deleted: Mapped[bool] = mapped_column(
-            Boolean,
-            default=False,
-            )
-    
+        Boolean,
+        default=False,
+    )
+
     # Связь с таблицей black_list
     black_list: Mapped[List['BlackList']] = relationship(
-            back_populates='user'
-            )
-
+        back_populates='user'
+    )
